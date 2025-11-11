b834246df86695219787bf4d32796bb9666a95d3
Akshay Patil <patilakshaya999@gmail.com>
2025-11-12 00:10:13 +0530

    Create missing-number.js

---
+++ missing-number.js
@@ -0,0 +1,11 @@
+var missingNumber = function (nums) {
+    const n = nums.length;
+    const sumOriginal = n * (n + 1) / 2;
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        sum = sum + nums[i]
+    }
+
+    return sumOriginal - sum;
+};
