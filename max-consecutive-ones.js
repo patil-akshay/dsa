d745addd4f63d854504eaeabfcfcb3569d07e6c8
Akshay Patil <patilakshaya999@gmail.com>
2025-11-11 23:59:11 +0530

    Create max-consecutive-ones.js

---
+++ max-consecutive-ones.js
@@ -0,0 +1,13 @@
+var findMaxConsecutiveOnes = function (nums) {
+    let curr = 0;
+    let max = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
+            curr++;
+        } else {
+            max = Math.max(curr, max);
+            curr = 0;
+        }
+    }
+    return Math.max(curr, max);
+};
