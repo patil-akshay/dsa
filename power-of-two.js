d243015e8a66fde5703edac3166385f3674b0830
Akshay Patil <patilakshaya999@gmail.com>
2025-11-12 23:15:47 +0530

    Create power-of-two.js

---
+++ power-of-two.js
@@ -0,0 +1,11 @@
+const n = 3
+var isPowerOfTwo = function(n) {
+    if(n == 1) {
+        return true;
+    } else if ( n % 2 != 0 || n < 1){
+        return false;
+    }
+    return isPowerOfTwo(n/2);
+};
+
+console.log(isPowerOfTwo(n))
