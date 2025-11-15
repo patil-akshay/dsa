09bd890242845c7c202854c7fd32a643b2ea16c1
Akshay Patil <patilakshaya999@gmail.com>
2025-11-15 17:35:38 +0530

    Create intersection-of-two-linked-lists.js

---
+++ intersection-of-two-linked-lists.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+    const check = new Set();
+    while (headB && headB.next) {
+        check.add(headB);
+        headB = headB.next;
+    }
+
+    while (headA && headA.next) {
+        if (check.has(headA)) {
+            return headA;
+        }
+        headA = headA.next;
+    }
+    return null;
+};
