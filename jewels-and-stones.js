2ff6c603c3dd773dfa14e9eab59c9c7fd90ac100
Akshay Patil <patilakshaya999@gmail.com>
2025-11-17 20:17:01 +0530

    Create jewels-and-stones.js

---
+++ jewels-and-stones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+    const check = new Set();
+    for (let i = 0; i < jewels.length; i++) {
+        check.add(jewels[i]);
+    }
+
+    let count = 0;
+    for (let i = 0; i < stones.length; i++) {
+        if (check.has(stones[i])) {
+            count++;
+        }
+    }
+    return count;
+};
