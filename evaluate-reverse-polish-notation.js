75804a3906a885ac02d52e3c94906c0417970ee0
Akshay Patil <patilakshaya999@gmail.com>
2025-11-25 22:35:09 +0530

    Create evaluate-reverse-polish-notation.js

---
+++ evaluate-reverse-polish-notation.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+    const stack = [];
+    for (let i = 0; i < tokens.length; i++) {
+        if (tokens[i] == '-' || tokens[i] == '+' || tokens[i] == '/' || tokens[i] == '*') {
+            let a = stack.pop();
+            let b = stack.pop();
+            let ans = eval(`${b} ${tokens[i]} ${a}`);
+            stack.push(Math.trunc(ans));
+        } else {
+            stack.push(tokens[i]);
+        }
+    }
+    //return Number
+    //return +stack.pop();
+    return Number(stack.pop());
+};
+
+
+
+function evalRPN(tokens) {
+  let stack = [];
+  let ops = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b >= 0 ? Math.floor(a / b) : Math.ceil(a / b),
+  };
+  for (let t of tokens) {
+    if (ops[t]) {
+      let top = stack.pop();
+      let second = stack.pop();
+      stack.push(ops[t](second, top));
+    } else {
+      stack.push(Number(t));
+    }
+  }
+  return stack.pop();
+};
