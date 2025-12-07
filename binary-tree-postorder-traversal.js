7b41e12e2b03622e2fc4e45c0c48c9b54ece32f0
Akshay Patil <patilakshaya999@gmail.com>
2025-12-07 11:03:04 +0530

    Update binary-tree-postorder-traversal.js

--- binary-tree-postorder-traversal.js
+++ binary-tree-postorder-traversal.js
@@ -56,3 +56,43 @@ var postorderTraversal = function (root) {
     }
     return ans;
 };
+
+
+// one stack
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+    if (!root) return [];
+    const ans = [];
+    const stack = [];
+    let curr = root;
+    let lastVisited = null;
+    while (curr || stack.length) {
+        while (curr) {
+            stack.push(curr);
+            // leftmost bottom element
+            curr = curr.left;
+        }
+        let peek = stack[stack.length - 1];
+        // top of stack
+        if (peek.right && peek.right !== lastVisited) {
+            curr = peek.right;
+        } else {
+            ans.push(peek.val);
+            lastVisited = stack.pop();
+        }
+    }
+    return ans;
+};
