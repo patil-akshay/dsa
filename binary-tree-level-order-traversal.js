742ea3566290bf67ae10caf8e6e710453bcb5e30
Akshay Patil <patilakshaya999@gmail.com>
2025-12-07 13:33:43 +0530

    Update binary-tree-level-order-traversal.js

--- binary-tree-level-order-traversal.js
+++ binary-tree-level-order-traversal.js
@@ -9,9 +9,13 @@
 /**
  * @param {TreeNode} root
  * @return {number[][]}
+
+ Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
  */
 
 // Time - Space - O(n)
+
 var levelOrder = function (root) {
     if (!root) return [];
     const ans = [];
@@ -36,3 +40,33 @@ var levelOrder = function (root) {
     }
     return ans;
 };
+
+
+//Using recurrsion
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+    if (!root) return [];
+    const ans = [];
+    function traversal(curr, level) {
+        if (!ans[level]) ans[level] = [];
+        ans[level].push(curr.val);
+        curr.left && traversal(curr.left, level + 1);
+        curr.right && traversal(curr.right, level + 1);
+    }
+    // pass level
+    traversal(root, 0);
+    return ans;
+};
+
