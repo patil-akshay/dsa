bcdf46e4536faf485c61b3e79f5bb27118ed45ef
Akshay Patil <patilakshaya999@gmail.com>
2025-12-08 20:29:48 +0530

    Update symmetric-tree.js

--- symmetric-tree.js
+++ symmetric-tree.js
@@ -28,3 +28,27 @@ var isSymmetric = function (root) {
     }
     return isMirror(root.left, root.right);
 };
+
+
+// Itreate approach
+
+var isSymmetric = function (root) {
+    if (!root) return false;
+    // compare left & right subtree
+    let q = [root.left, root.right];
+
+    while (q.length) {
+        let p1 = q.shift();
+        let p2 = q.shift();
+
+        if (!p1 && !p2) continue;
+
+        if (!p1 || !p2) return false;
+
+        if (p1.val !== p2.val) return false;
+
+        q.push(p1.right, p2.left);
+        q.push(p1.left, p2.right);
+    }
+    return true;
+};
