b5452a5dc72e65262230ecc38f89496e977934fe
Akshay Patil <patilakshaya999@gmail.com>
2025-12-08 21:20:57 +0530

    Create binary-tree-zigzag-level-order-traversal.js

---
+++ binary-tree-zigzag-level-order-traversal.js
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+    if (!root) return [];
+    let q = [root];
+    let ans = [];
+    let level = 0;
+    while (q.length) {
+        let levelArr = [];
+        let levelSize = q.length;
+        for (let i = 0; i < levelSize; i++) {
+            let curr = q.shift();
+            if (level % 2 !== 0) {
+                // reverse order
+                levelArr.unshift(curr.val);
+            } else {
+                levelArr.push(curr.val)
+            }
+            curr.left && q.push(curr.left);
+            curr.right && q.push(curr.right);
+        }
+        ans.push(levelArr);
+        level++;
+    }
+    return ans;
+};
+
+
+
+// Recursive Approach
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+    if (!root) return [];
+    const ans = [];
+    function traversal(curr, level) {
+        if (!ans[level]) ans[level] = [];
+        if (level % 2 !== 0) {
+            // reverse order
+            ans[level].unshift(curr.val);
+        } else {
+            ans[level].push(curr.val)
+        }
+        curr.left && traversal(curr.left, level + 1);
+        curr.right && traversal(curr.right, level + 1);
+    }
+    // pass level
+    traversal(root, 0);
+    return ans;
+};
